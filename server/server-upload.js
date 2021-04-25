26581701e8dba922d6f6fb2ebe34c4f30a340371
best <sunsasabest@hotmail.com>
2021-04-26 00:11:36 +0700

    [ADD] br

--- server/server-upload.js
+++ server/server-upload.js
@@ -3,14 +3,14 @@ const multer = require('multer');
 const execSync = require('child_process').execSync;
 var fs = require('fs');
 
-var fileName="";
+var fileName = "";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads');
     },
-    filename: (req, file, cb) =>{
-        const {originalname} =file;
+    filename: (req, file, cb) => {
+        const { originalname } = file;
         fileName = file.originalname;
         cb(null, originalname);
     }
@@ -23,78 +23,79 @@ app.use(express.static('public'));
 
 
 app.post('/upload', upload.single('avatar'), (req, res) => {
-    execSync(`cp ${req.file.path} ~/inputs`, (e, stdout, stderr)=>{
-        if(e instanceof Error){
+    execSync(`cp ${req.file.path} ~/inputs`, (e, stdout, stderr) => {
+        if (e instanceof Error) {
             console.error(e);
         }
-	console.log('KUY');
+        console.log('KUY');
         console.log(stdout);
         console.log('stderr', stderr);
     });
-    execSync(`rm ${req.file.path}`, (e, stdout, stderr)=>{
-        if(e instanceof Error){
-            console.error(e);
-        }
-        console.log(stdout);
-        console.log('stderr', stderr);
-    });
-execSync(`~/hadoop/hadoop-3.1.4/bin/hadoop fs -mkdir /input`, (e, stdout, stderr)=>{
-        if(e instanceof Error){
+    execSync(`rm ${req.file.path}`, (e, stdout, stderr) => {
+        if (e instanceof Error) {
             console.error(e);
         }
         console.log(stdout);
         console.log('stderr', stderr);
     });
-    execSync(`~/hadoop/hadoop-3.1.4/bin/hadoop fs -put ~/inputs/${fileName} /input`, (e, stdout, stderr)=>{
-        if(e instanceof Error){
+    execSync(`~/hadoop/hadoop-3.1.4/bin/hadoop fs -mkdir /input`, (e, stdout, stderr) => {
+        if (e instanceof Error) {
             console.error(e);
         }
         console.log(stdout);
         console.log('stderr', stderr);
     });
-    execSync(`cd ~ && ~/hadoop/hadoop-3.1.4/bin/hadoop jar WordCount.jar WordCount /input /output`, (e, stdout, stderr)=>{
-       if(e instanceof Error){
-            console.error(e);
-        }
-	console.log('KU JA BA TAI');
-        console.log(stdout);
-        console.log('stderr', stderr);
-    });
-    execSync(`cd ~ && ~/hadoop/hadoop-3.1.4/bin/hadoop fs -cat /output/part-r-00000 > temp`, (e, stdout, stderr)=>{
-	console.log('Hey');       
- if(e instanceof Error){
-            console.error(e);
-        }
-	if(stderr){
-	console.log(stderr);
-}
-	console.log('hello');
-        console.log(stdout);
-    //   return res.json({ status: 'OK', message: stdout });
-        console.log('stderr', stderr);
-    }); 
-execSync(`~/hadoop/hadoop-3.1.4/bin/hadoop fs -rm -R /input`, (e, stdout, stderr)=>{
-        if(e instanceof Error){
+    execSync(`~/hadoop/hadoop-3.1.4/bin/hadoop fs -put ~/inputs/${fileName} /input`, (e, stdout, stderr) => {
+        if (e instanceof Error) {
             console.error(e);
         }
         console.log(stdout);
         console.log('stderr', stderr);
     });
-execSync(`~/hadoop/hadoop-3.1.4/bin/hadoop fs -rm -R /output`, (e, stdout, stderr)=>{
-        if(e instanceof Error){
+    execSync(`cd ~ && ~/hadoop/hadoop-3.1.4/bin/hadoop jar WordCount.jar WordCount /input /output`, (e, stdout, stderr) => {
+        if (e instanceof Error) {
+            console.error(e);
+        }
+        console.log('KU JA BA TAI');
+        console.log(stdout);
+        console.log('stderr', stderr);
+    });
+    execSync(`cd ~ && ~/hadoop/hadoop-3.1.4/bin/hadoop fs -cat /output/part-r-00000 > temp`, (e, stdout, stderr) => {
+        console.log('Hey');
+        if (e instanceof Error) {
+            console.error(e);
+        }
+        if (stderr) {
+            console.log(stderr);
+        }
+        console.log('hello');
+        console.log(stdout);
+        //   return res.json({ status: 'OK', message: stdout });
+        console.log('stderr', stderr);
+    });
+    execSync(`~/hadoop/hadoop-3.1.4/bin/hadoop fs -rm -R /input`, (e, stdout, stderr) => {
+        if (e instanceof Error) {
             console.error(e);
         }
         console.log(stdout);
         console.log('stderr', stderr);
     });
-fs.readFile('/home/hadoop/temp', 'utf8', function(err, data) {
-    if (err) throw err;
-    console.log(data);
-   res.set('Content-Type', 'text/html');
-res.send(Buffer.from(data));
-});
-     console.log(fileName);
-//	return res.json({status: 'OK'});    
+    execSync(`~/hadoop/hadoop-3.1.4/bin/hadoop fs -rm -R /output`, (e, stdout, stderr) => {
+        if (e instanceof Error) {
+            console.error(e);
+        }
+        console.log(stdout);
+        console.log('stderr', stderr);
+    });
+    fs.readFile('/home/hadoop/temp', 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        res.set('Content-Type', 'text/html');
+        res.send(Buffer.from(data.replace("/n","<br>")));
+    });
+    console.log(fileName);
+    //	return res.json({status: 'OK'});    
+    
 });
 
 
